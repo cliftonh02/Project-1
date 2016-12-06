da97eab43f8222f8e3cdd0e79187442e8831b151
yourUsername <your_email@example.com>
2016-12-06 08:39:38 -0500

    Project update

---
+++ quiz-answers.js
@@ -0,0 +1,10 @@
+function quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+
+quiz.prototype.getQuestionIndex = function(){
+  return this.questions{this.questionIndex}
+
+};
